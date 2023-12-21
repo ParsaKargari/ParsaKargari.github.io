25c6c0c734c999dd59158920678f0a1400c46a39
Parsa Kargari <parsa.kargari@ucalgary.ca>
2023-12-20 22:59:25 -0700

    Test: Deployment Workflow Test
    Signed-off-by: Parsa Kargari <parsa.kargari@ucalgary.ca>

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Container from "@mui/material/Container";
 
+
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
     // Check for dark mode preference in localStorage
